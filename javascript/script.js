5cbe97f1e4e1690f979368ab22fe57d1a5b359b6
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-10-02 19:34:11 +0100

    further styling

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,18 @@
+const billAmount = document.getElementById("bill");
 const tipAmount = document.getElementById("tip-amount");
 const totalAmount = document.getElementById("total-amount");
 const customTip = document.getElementById("custom-tip");
+
+billAmount.addEventListener("keyup", () => {
+  totalAmount.innerHTML = parseFloat(billAmount.value).toFixed(2);
+})
 
 customTip.addEventListener("keyup", () => {
   tipAmount.innerHTML = parseFloat(customTip.value).toFixed(2);
 });
 
-console.log(typeof customTip.innerHTML);
+
+
+
+
+

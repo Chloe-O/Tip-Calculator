b9dc4c7a383be8460174839644b748d652bcf3c2
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-10-03 19:40:08 +0100

    logic

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,56 @@
-const billAmount = document.getElementById("bill");
-const tipAmount = document.getElementById("tip-amount");
-const totalAmount = document.getElementById("total-amount");
-const customTip = document.getElementById("custom-tip");
+const billInput = document.getElementById("bill");
+const customTipInput = document.getElementById("custom-tip");
+const peopleNumInput = document.getElementById("people-num");
+const tipAmountDisplay = document.getElementById("tip-amount");
+const totalAmountDisplay = document.getElementById("total-amount");
+let clearFormBtn = document.getElementById("clear-form");
 
-billAmount.addEventListener("keyup", () => {
-  totalAmount.innerHTML = parseFloat(billAmount.value).toFixed(2);
+// let Inputs = document.querySelectorAll(".input");
+
+billInput.addEventListener("change", () => {
+  if (customTipInput.value != "" && peopleNumInput.value != "") {
+    console.log("hello");
+  }
 });
 
-customTip.addEventListener("keyup", () => {
-  tipAmount.innerHTML = parseFloat(customTip.value).toFixed(2);
+customTipInput.addEventListener("change", () => {
+  if (billInput.value != "" && peopleNumInput.value != "") {
+    console.log("world");
+  }
 });
+
+// Inputs.forEach((i) => {
+//   i.addEventListener("click", () => {
+//     calculateTotal();
+//   });
+// });
+
+// function calculateTotal() {
+//   let customTip;
+//   let billAmount;
+//   let peopleNum;
+//   let total;
+
+//   customTipInput.addEventListener("change", () => {
+//     customTip = parseFloat(customTipInput.value).toFixed(2);
+//   });
+
+//   billInput.addEventListener("change", () => {
+//     billAmount = billInput.value;
+//   });
+
+//   peopleNumInput.addEventListener("change", () => {
+//     peopleNum = peopleNumInput.value;
+//   });
+
+// }
+
+function resetForm() {
+  billInput.value = "";
+  customTipInput.value = "";
+  peopleNumInput.value = "";
+  tipAmountDisplay.innerHTML = "0.00";
+  totalAmountDisplay.innerHTML = "0.00";
+}
+
+clearFormBtn.addEventListener("click", resetForm);
